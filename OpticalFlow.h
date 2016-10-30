272d06a17eeb5df8fd5cfcba82cb22498afa8d91
caladanY <caladanY@caladanY.github.io>
2016-10-30 16:34:46 +0800

    remove unnecessary files

--- OpticalFlow.h
+++
@@ -1,4 +0,0 @@
-extern void trackFeatures(const cv::Mat &img_l, const cv::Mat &img_r, std::vector<cv::Point2f> &features_l, std::vector<cv::Point2f> &features_r, std::vector<int> &status,
-                          int stereo);
-
-
