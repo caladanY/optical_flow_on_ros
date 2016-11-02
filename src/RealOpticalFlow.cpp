4e1f85156def303897b28ac78ad7fbc906b6eac2
caladanY <caladanY@caladanY.github.io>
2016-11-02 13:34:45 +0800

    using klt-track-feature

--- src/RealOpticalFlow.cpp
+++ src/RealOpticalFlow.cpp
@@ -18,14 +18,29 @@ using namespace cv;
 extern void trackFeatures(const cv::Mat &img_l, const cv::Mat &img_r, std::vector<cv::Point2f> &features_l, std::vector<cv::Point2f> &features_r, std::vector<int> &status, int stereo);
 
 
+
 std::vector<cv::Mat> imgSeq;
+std::vector<cv::Point2f> features;
+std::vector<int> status;
+std::vector<cv::Point2f> features_r;
+std::vector<cv::Point2f> pre_features;
+cv::Mat imgResult;
+cv::Mat img_r;
+
+
 
 void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 {
   try
   {
     imgSeq.push_back(cv_bridge::toCvShare(msg, "bgr8")->image);
-    cv::imshow("view", imgSeq.back());
+    trackFeatures(imgSeq.back(),img_r,features,features_r,status,0);
+    imgResult=imgSeq.back();
+    for(int j = imgSeq.size() - 1; j > (imgSeq.size() - 10); j--){
+      if(j < 0) break;    
+      arrowedLine(imgResult,features[j],pre_features[j],CV_RGB(255,0,0),2,8,0);
+    }
+    cv::imshow("view", imgResult);
     cv::waitKey(30);
   }
   catch (cv_bridge::Exception& e)
@@ -38,6 +53,11 @@ void imageCallback(const sensor_msgs::ImageConstPtr& msg)
 
 int main(int argc, char *argv[])
 {
+    status.resize(100);
+    for(int i=0; i<100;i++){
+      status[i]=2;
+    }
+
     ros::init(argc, argv, "optical_process");
     ros::NodeHandle nh;
     cv::namedWindow("view");
